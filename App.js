72de9bed7396cb0b6562cd84f061dccf9771fe85
Thân Anh Kiên <kientts@MacBook-Air-cua-Than.local>
2020-05-22 17:55:39 +0700

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+var app = require('express')(),
+   port = process.env.PORT || 3000;
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + "/demo.html");
+})
+
+app.listen(port, function() {
+  console.log('Listening on port ' + port)
+})
